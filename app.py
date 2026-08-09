764119bbfa7d33d94f0476714dfac3bb1a462e11
nikhilbarde6161 <bardenikhil6161@gmail.com>
2026-08-09 13:32:57 +0530

    Add health check route to Flask app

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "Hello from DevOps GitOps Pipeline!"
 
+
 @app.route("/health")
 def health():
     return "OK"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
